e63476da5428c6b4276ddd42236938900ab127ce
Avi Tamrakar <abis39112@gmail.com>
2025-08-21 17:05:17 +0530

    all pages working

--- src/pages/GameStoreManagerPage.jsx
+++ src/pages/GameStoreManagerPage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import GameStoreManager from "../components/GameStoreManager";
+import GameStoreManager from "../components/game-store-manager/GameStoreManager";
 
 export default function GameStoreManagerPage() {
   return <GameStoreManager />;
